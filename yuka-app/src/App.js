e98071a6a0d42177e999bb9203d25776ea5801e1
debbieyuen <deborahyuen2016@berkeley.edu>
2023-04-21 23:40:42 -0700

    add yuka art work vectors

--- yuka-app/src/App.js
+++ yuka-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+// Import GSAP
+import React from "react";
+import {gsap} from "gsap";
+import { Draggable, InertiaPlugin} from 'gsap/all'
+import {useRef, useEffect} from "react";
 import './App.css';
+import './Vectors.css';
 
-function App() {
+gsap.registerPlugin(Draggable);
+gsap.registerPlugin(InertiaPlugin);
+
+// Vectors
+// Source: https://legacy.reactjs.org/docs/composition-vs-inheritance.html
+function Row(props) {
+  const imgRef = useRef(null);
+  useEffect(() => {
+    const element = imgRef.current;
+    Draggable.create(element, {
+      type: "x,y",
+      inertia: true,
+      dragClickables: true,
+      bounds: window
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className={props.vector} ref={imgRef}></div>
   );
 }
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+      <h2>Yuka Kobayashi</h2>
+          <Row vector="large1"></Row>
+          <Row vector="large2"></Row>
+          <Row vector="large3"></Row>
+          <Row vector="large4"></Row>
+          <Row vector="large5"></Row>
+          <Row vector="large6"></Row>
+          <Row vector="large7"></Row>
+          <Row vector="medium1"></Row>
+          <Row vector="medium2"></Row>
+          <Row vector="medium3"></Row>
+          <Row vector="medium4"></Row>
+          <Row vector="medium5"></Row>
+          <Row vector="medium6"></Row>
+          <Row vector="small1"></Row>
+          <Row vector="small2"></Row>
+          <Row vector="small3"></Row>
+        </header>  
+    </div>
+
+    </>
+    
+  );
+}
+
 
 export default App;
